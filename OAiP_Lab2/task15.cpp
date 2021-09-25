1f676ea95342be486bdc28965777b47cbab203a0
panton8 <podvalnikov0@gmail.com>
2021-09-25 23:42:43 +0300

    Add current state of Lab2

--- OAiP_Lab2/task15.cpp
+++ OAiP_Lab2/task15.cpp
@@ -12,15 +12,19 @@ int main()
     cin >> K;
     if (K < 0)
     {
-        cout << "ERROR";
+        cout << "ERROR!";
     }
-    else if (K == round(K))
+    else if(((int(K) % 10 == 2) || (int(K) % 10 == 3) || (int(K) % 10 == 4 )) || (K != round(K)))
     {
-        cout << "Mi nashli" << " " << K << " " <<"gribov";
+        cout << "Mi nashli" << " " << K << " " << "griba v lesy";
     }
-    else
+    else if ((K == 1) || ((K >= 21) &&  ((int(K) % 10 == 1))) )
     {
-        cout << "Mi nashli" << " " << K << " " << "griba";
+        cout << "Mi nashli" << " " << K << " " << "grib v lesy";
+    }
+    else if ((K == round(K)) && ((int(K) % 10 != 1) || (int(K) % 10 != 2) || (int(K) % 10 != 3) || (int(K) % 10 !=4)))
+    {
+        cout << "Mi nashli" << " " << K << " " << "gribov v lesy";
     }
     return 0;
 }
