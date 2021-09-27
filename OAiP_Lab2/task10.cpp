db0ccbdcf4cff5e45f5bafe3c2da858e442aff9f
panton8 <podvalnikov0@gmail.com>
2021-09-27 22:30:05 +0300

    Update task 10 of Lab2

--- OAiP_Lab2/task10.cpp
+++ OAiP_Lab2/task10.cpp
@@ -3,7 +3,6 @@
 при заданном х, определить и вывести на экран дисплея минимальное
 значение. Библиотеку <cmath> использовать запрещено.*/
 #include <iostream>
-#define e 2.71
 
 using namespace std;
 
@@ -11,28 +10,33 @@ int main()
 {
     double x, a, b, c;
     cin >> x;
+    double y = x*x;
+    double z = x*x*x;
+    if (x < 0)
+    {
+        x = -x;
+        double ln_x = x - ( y/2) + (z/6) - (y*y /24) + (z*y / 120) - (z*z / 720) + (z*z*y/40320);;
+        c = ln_x;
+    }
     if (x > 6.28)
     {
         x = x - (6.28 *(int(x / 6.28)));
     }
-    double sin_x = x - ((x * x * x) / (1 * 2 * 3)) + ((x * x * x * x * x) / (1 * 2 * 3 * 4 * 5)) -((x * x * x * x * x * x * x) / (1 * 2 * 3 * 4 * 5 * 6 * 7));
-    double cos_x = 1 - ((x * x) / (1 * 2)) + ((x * x * x * x ) / ( 1 * 2 * 3 * 4)) -((x * x * x * x * x * x )/ (1 * 2 * 3 * 4 * 5 * 6));
-    x > 0 ? x = x : x = -x;
-    double ln_x = 0;
+    double sin_x = x - (z / 6) + ((z*y) / 120) - ((z*y*y) / 5040) + ((z*z*z)/362880) - ((z*z*z*y) / 39916800) + ((z*z*z*y*y)/ 39916800);
+    double cos_x = 1 - ((y) / 2 ) + ((y*y) / 24) - ((z*z)/ (720)) + ((z*z*y)/40320) - ((z*z*y*y)/3628800) + ((z*z*z*z)/479001600) - (z*z*z*z*y/87178291200);
     a = sin_x;
     b = cos_x;
-    c = ln_x;
-    if (a < b && a < c)
+    if ((a < b) && (a < c))
     {
-        cout << a;
+        cout << "sin(x) (min) = "  << a;
     }
-    else if (b < a && b < c)
+    else if ((b < a) && (b < c))
     {
-        cout << b;
+        cout << "cos(x) (min) = " << b;
     }
-    else if (c < a && c < b)
+    else if ((c < a) && (c < b))
     {
-        cout << c;
+        cout << "ln|x| (min) = " << c;
     }
     return 0;
 }
