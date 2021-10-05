eadcd66884e819e29aede7cce395a79c04288d3c
panton8 <podvalnikov0@gmail.com>
2021-10-06 00:53:48 +0300

    Add task1 of Lab3

---
+++ OAiP_Lab3/task1.cpp
@@ -0,0 +1,20 @@
+//Лабораторная 3, задача 1. Выполнена: Подвальников А.С.
+/*(использовать while) Вычислить сумму,где a и b определены по формуле.*/
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int i = 1;
+    int sum = 0;
+    int a,b;
+    while(i <= 30)
+    {
+        i++;
+        i % 2 == 1 ? a = i, b = i*i : a = i/2,b = i*i*i;
+        sum += pow(a-b,2);
+    }
+    std::cout << "Sum of 30 terms = " << sum;
+    return 0;
+}
