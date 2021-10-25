52ff3d8fa7dd279bbbb72f828a44cebdb9a28914
panton8 <podvalnikov0@gmail.com>
2021-10-25 20:55:06 +0300

    Fix task1 of Lab4

--- OAiP_Lab4/task1.cpp
+++ OAiP_Lab4/task1.cpp
@@ -1,33 +1,55 @@
 //Лабораторная 1, задача 1(1). Выполнена: Подвальников А.С.
-/**/
+/*Ввести одномерный статический массив из k чисел.Преобразовать массив
+следующим образом:все отрицательные элементы массива пенести в начало,
+ сохранив исходное взаимное расположение,как средиотрицательных,так и
+ среди остальных элементов массива.*/
 
 #include <iostream>
+#include <cmath>
+
 int main()
 {
-    int k;
-    std::cout << "Enter k" << std::endl;
-    std::cin >> k;
-    double massive [k];
-    std::cout << "Enter terms of massive" <<std::endl;
-    for(int i = 0; i < k ; i++)
-        std::cin >> massive[i];
-    std::cout << "Your massive : ";
-    for(int i = 0; i < k ; i++)
-        std::cout << massive[i] << " ";
-    int j = 0 ;
-    double temp;
-    for(int i = 0; i <= k-1 ; i++)
+    int k = 0;
+    std::cout << "Enter size of array: " << std::endl;
+    while(true)
     {
-        if(massive [i] < 0)
+        std::cin >> k;
+        if(std::cin.fail() ||  k != (long long)k || k != round(k))
         {
-            temp = massive[i];
-            massive [i] = massive [j];
-            massive[j] = temp;
-            j++;
+            std::cout <<"Incorrect type of variable! Please,enter your value again: " << std::endl;
+            std::cin.clear();
+            std::cin.ignore(32000,'\n');
+            continue;
         }
+        break;
     }
-    std::cout << std::endl << "New view of massive is : ";
-    for (j = 0; j <= k - 1 ; j++)
-        std::cout << massive[j] << " ";
+    int arr [k];
+    std::cout << "Enter terms of array: " <<std::endl;
+    for(int i = 0; i < k ; i++)
+        while(true)
+        {
+            std::cin >> arr[i];
+            if(std::cin.fail() ||  i != (long long)i || i!= round(i))
+            {
+                std::cout <<"Incorrect type of variable! Please,enter your value again: " << std::endl;
+                std::cin.clear();
+                std::cin.ignore(32000,'\n');
+                continue;
+            }
+            break;
+        }
+    std::cout << "Your array : ";
+    for(int i = 0; i < k ; i++)
+        std::cout << arr[i] << " ;";
+    for(int i = 0; i < k - 1; i++)
+    {
+        for (int j=0; j<k-1; j++)
+            if (arr[j] >=0 && arr[j+1]<0)
+                std::swap(arr[j], arr[j+1]);
+
+    }
+    std::cout << std::endl << "New view of array is : ";
+    for (int j = 0; j < k ; j++)
+        std::cout << arr[j] << " ;";
     return 0;
 }
