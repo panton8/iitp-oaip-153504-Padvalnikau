ff626aa895005e6b6dd7361e0939c5b5fd9b9424
panton8 <podvalnikov0@gmail.com>
2021-10-29 23:23:43 +0300

    Add task13 of Lab4

---
+++ OAiP_Lab4/task13.cpp
@@ -0,0 +1,42 @@
+//Лабораторная 4, задача 13. Выполнена: Подвальников А.С.
+/*У Миши в общежитии всего лишь 1 розетка. Дабы исправить это
+недоразумение, Миша купил N удлинителей таких, что i-й удлинитель имеет
+a[i] входов. Вычислите, сколько розеток получится у Миши, если он
+оптимально соединит удлинители?.*/
+
+#include <iostream>
+#include <cmath>
+#include <time.h>
+
+int main()
+{
+    int  n, sum = 0;
+    srand(time(NULL));
+    std::cout << "Num of extension cords: " << std::endl;
+        while(true)
+        {
+            std::cin >> n;
+            if(std::cin.fail() ||  n != (long long)n || n!= round(n))
+            {
+                std::cout <<"Incorrect type of variable! Please,enter your value again: " << std::endl;
+                std::cin.clear();
+                std::cin.ignore(32000,'\n');
+                continue;
+            }
+            break;
+        }
+    int *arr = new int[n];
+    for (int i = 0; i < n; ++i) {
+        arr[i] = 1 + rand() % 10;
+    }
+    std::cout << "Extension cords have inputs: " << std::endl;
+    for(int i = 0; i < n; i++)
+    {
+        std::cout << arr[i] << "; ";
+    }
+    for (int i = 0; i < n; ++i)
+            sum += arr[i] - 1 ;
+    std::cout << std::endl <<"Sockets: " << sum + 1;
+    delete[] arr;
+    return 0;
+}
