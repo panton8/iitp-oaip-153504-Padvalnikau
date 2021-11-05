602f2ec7ed4f711414e68d2c72889a4ade0c8603
panton8 <podvalnikov0@gmail.com>
2021-11-05 22:46:52 +0300

    Update task12 of Lab4

--- OAiP_Lab4/task12.cpp
+++ OAiP_Lab4/task12.cpp
@@ -4,32 +4,37 @@
 параметры: новый массив, новый размер.*/
 #include <iostream>
 #include <time.h>
-
-void RepDel(int *arr,int n)
+int n2 ;
+int* RepDel(int *arr,int n)
 {
-    srand(time(nullptr));
-    std::cout << "Array:v" << std::endl;
-    for (int i = 0; i < n; ++i) {
-        arr[i] = 1 + rand() % 10;
-        std::cout << " " << arr[i];
-    }
-    std::cout << std::endl;
-    for (int i = 0; i < n - 1; i++)
-        for (int j = i + 1; j < n; j++)
-            if (arr[i] == arr[j])
-            {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
                 for (int k = j; k < n; k++)
                     arr[k] = arr[k + 1];
                 n--;
                 j--;
             }
-    std::cout << "Array without duplicates: " << std::endl;
-    for (int i = 0; i < n; i++)
-        std::cout << arr[i]<< " ";
+        }
+    }
+    n2 = n;
+    return arr;
 }
-int main()
-{
-    int *arr = new int[45];
-    RepDel(arr,45);
+int main() {
+    int n;
+    std::cin >> n;
+    int *arr = new int[n];
+    srand(time(nullptr));
+    std::cout << "Array: " << std::endl;
+    for (int i = 0; i < n; ++i) {
+        arr[i] = 1 + rand() % 10;
+        std::cout << " " << arr[i];
+    }
+    std::cout << std::endl << "Array without duplicates: " << std::endl;
+    RepDel(arr,n);
+    for(int i = 0;i < n2;i++) {
+    std::cout << arr[i] << " ";
+    }
     delete[] arr;
+    return 0;
 }
