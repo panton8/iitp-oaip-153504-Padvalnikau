9b50d8f22387fb9f981d9bb4163be903bb1a7578
panton8 <podvalnikov0@gmail.com>
2021-12-03 15:28:41 +0300

    Add static_lib of Lab5

--- OAiP_Lab5/static_lib.h
+++ OAiP_Lab5/static_lib.h
@@ -1,16 +1,11 @@
+#ifndef STATIC_LIB_INCLUDED
+#define STATIC_LIB_INCLUDED
+
+namespace solution{
+    double equation(double a,double b);
+}
+
+
+#endif //STATIC_LIB_INCLUDED
+
 //
-// Created by podva on 10.11.2021.
-//
-
-#ifndef OAIP_LAB5_STATIC_LIB_H
-#define OAIP_LAB5_STATIC_LIB_H
-
-
-
-class static_lib {
-
-};
-
-
-
-#endif //OAIP_LAB5_STATIC_LIB_H
