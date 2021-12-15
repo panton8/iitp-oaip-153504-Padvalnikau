3dfb9a741b86e5d532ede4ead77ed38cac370cfd
panton8 <podvalnikov0@gmail.com>
2021-12-15 21:44:04 +0300

    Add task2 for Lab7

---
+++ OAiP_Lab7/task2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+std::string reverseCode(std::string straight_code){
+    std::string reverse_code;
+    int str_size = 0;
+    for(int i = 0; straight_code[i] != '\0'; i++)
+        str_size++;
+    for(int i = 0; i < str_size; i++)
+    {
+        if(i == 0)
+            reverse_code += straight_code[0];
+        if(straight_code[0] == '0' && i > 0)
+            reverse_code += straight_code[i];
+        else
+            if( i > 0 && straight_code[i] == '0')
+                reverse_code += '1';
+            else if( i > 0 && straight_code[i] == '1')
+                reverse_code += '0';
+    }
+    return reverse_code;
+}
+
+int main(){
+    std::string straight_code;
+    std::cout << "Enter the straight code: " << std::endl;
+    std::cin >> straight_code;
+    std::string reverse_code = reverseCode(straight_code);
+    std::cout << reverse_code;
+    return 0;
+}
